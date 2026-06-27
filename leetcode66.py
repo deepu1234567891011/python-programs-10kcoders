2968a94816a77ac651670f69ab4328d7ca439422
vicharapu srideepika <deepikavicharapu@gmail.com>
2026-06-27 21:39:05 +0530

    Add files via upload

---
+++ leetcode66.py
@@ -0,0 +1,23 @@
+class Solution:
+    def plusOne(self, digits):
+        for i in range(len(digits)-1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+
+            digits[i] = 0
+
+        return [1] + digits
+
+
+# Create object
+obj = Solution()
+
+# Test input
+digits = [1, 2, 3]
+
+# Call function
+result = obj.plusOne(digits)
+
+# Print result
+print(result)
