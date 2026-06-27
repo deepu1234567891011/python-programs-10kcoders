c39100061b0644aa1ac2ac10cd4183f8c9578625
vicharapu srideepika <deepikavicharapu@gmail.com>
2026-06-27 21:38:21 +0530

    Add files via upload

---
+++ bitwise operator.py
@@ -0,0 +1,29 @@
+# XOR
+a=3
+b=4
+print(a^b)
+# &AND
+a=5
+b=3
+print(a&b)
+
+# OR
+a=5
+b=3
+print(a|b)
+
+# Not Shift
+b=5
+print(~b)
+# LEFTSHIFT
+a=3
+print(a<<1)
+# Right shift
+a=5
+print(a>>1)
+# EVEN OR ODD
+n = int(input("Enter a Number:"))
+if n & 1:
+    print("odd")
+else:
+    print("even")
