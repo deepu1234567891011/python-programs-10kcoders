760b5aa02ca1d91f380c7edfd3472acecb3678e6
vicharapu srideepika <deepikavicharapu@gmail.com>
2026-06-27 21:39:51 +0530

    Add files via upload

---
+++ type conversions.py
@@ -0,0 +1,131 @@
+# int -float
+# a=30
+# b=float(a)
+# print(b)
+
+# #float-int
+# b=15.5
+# c=int(b)
+# print(c)
+
+# # int-string
+
+# a=30
+# b=int("30")
+# print(b)
+
+# #string -int
+# a=30
+# b=str("30")
+# print(b)
+
+# # float-str
+# a=10.5
+# b=str("10.5")
+# print(b)
+# print(b)
+
+# # str-float
+# a=10.5
+# b=float(15.5)
+# print(b)
+
+# #float-str
+# a=10.5
+# b=str("15.5")
+# print(b)
+
+# # int-bool
+# a=10
+# b=bool(a)
+# print(b)
+# print(type(b))
+
+# # #bool-int
+# a=10
+# b=int(a)
+# print(b)
+# print(type(b))
+
+# #float-bool
+# a=10.5
+# b=float(a)
+# print(b)
+# print(type(b))
+
+# # bool-float
+# a=10
+# b=bool(a)
+# print(b)
+# print(type(b))
+
+# # list-tuple
+# a=[1,2,3]
+# b=tuple(a)
+# print(b)
+# print(type(b))
+
+# # tuple-list
+# a=(1,2,3)
+# b=list(a)
+# print(b)
+# print(type(b))
+
+# # list-set
+# a=[1,2,3,4,5]
+# b=set(a)
+# print(b)
+# print(type(b))
+
+# # set-list
+# a={21,22,23,24}
+# b=list(a)
+# print(b)
+# print(type(b))
+
+# # tuple-set
+# a=(1,2,3,4)
+# b=set(a)
+# print(b)
+# print(type(b))
+
+# # set-tuple
+ a = {1, 2, 3}
+ b = tuple(a)
+ print(b)
+ print(type(b))
+
+# # str-list
+ a="arjun"
+ b=list(a)
+ print(b)
+ print(type(b))
+
+# # list-str
+ a = ['a','r','j','u','n']
+ b = "".join(a)
+ print(b)
+ print(type(b))
+
+# # str-tuple
+ a = "hello"
+ b = tuple(a)
+ print(b)
+ print(type(b))
+
+# # str - set
+ a = "hello"
+ b = set(a)
+ print(b)
+ print(type(b))
+
+# # dict → list
+ a = {'a': 1, 'b': 2}
+ b = list(a)
+ print(b)
+ print(type(b))
+# # dict → tuple
+ a = {'a': 1, 'b': 2}
+ b = tuple(a)
+ print(b)
+ print(type(b))
