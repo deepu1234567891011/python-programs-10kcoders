ff8abcd2e79057b40ab052784c189f34b4704881
vicharapu srideepika <deepikavicharapu@gmail.com>
2026-06-27 22:01:54 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,26 @@
+print(2&3)
+print(10&5)
+print(10|5)
+print(7&1)
+print(7|1)
+print(6&3)
+print(6|3)
+print(6^3)
+n=13
+print(n&(n-1)==0)
+x=10
+print(x>>3)
+# perform xor if two values are same what will be the output
+a=95
+b=95
+print(a^b)
+c=8
+d=8
+print(c^d)
+#perform and opt with even number and odd numbers
+a=8
+b=16
+print(a&b,"even numbers of and operation")
+c=1
+d=5
+print(c&d,"odd numbers of and opertion")
