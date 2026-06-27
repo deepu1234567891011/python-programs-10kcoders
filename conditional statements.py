1ea2eb71fc4098ffbe8b15b00c311bdc07ec19a5
vicharapu srideepika <deepikavicharapu@gmail.com>
2026-06-27 21:46:06 +0530

    Add files via upload

---
+++ conditional statements.py
@@ -0,0 +1,26 @@
+# num =int(input("Enter a number:"))
+   if num > 0:
+     print("Postive")
+ else:
+     print("Negative")
+
+#2. Check Character is Uppercase or Lowercase (Without Built-in Functions)
+  ch = input("Enter a character:")
+ if 'a' <=ch <='z':
+     print("Lowercase")
+ else:
+     print("Uppercaase")
+
+# 3. Pass or Fail (6 Subjects)
+s1=int(input("suject 1: "))
+s1=int(input("suject 2: "))
+s1=int(input("suject 3: "))
+s1=int(input("suject 4: "))
+s1=int(input("suject 5: "))
+s1=int(input("suject 6: "))
+
+if s1>=35 and s2>=35 and s3>=35 and s4>=35 and s5>=35 and s6<=35:
+    print("Pass")
+else:
+    print("Fail")
+
---
+++ nested loops.py
@@ -0,0 +1,7 @@
+rows = 12
+columns = 12
+
+for i in range(rows):
+    for j in range(columns):
+        print("*", end=" ")
+    print()
